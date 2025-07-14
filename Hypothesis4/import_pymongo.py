833bb8026ec0c66318ab3465b15ca38339543387
naoya526 <naoya.kumakura2003@gmail.com>
2025-07-14 17:42:51 +0200

    update clean mode

--- Hypothesis4/import_pymongo.py
+++ Hypothesis4/import_pymongo.py
@@ -1,10 +1,20 @@
 import pandas as pd
 from pymongo import MongoClient
+import sys
 
-# MongoDB接続
+# Check if the script is run with the --clean option
+clean_mode = "--clean" in sys.argv
+
+# MongoDB connection
 client = MongoClient("mongodb://localhost:27017/")
-db = client["game_reviews_db"]              # データベース名
-collection = db["clean_sample_reviews"]     # コレクション名
+db = client["game_reviews_db"]              # database name
+collection = db["clean_sample_reviews"]     # collection name
+
+# --clean option was selected
+if clean_mode:
+    print("--clean option was selected...deleting existing data in 'clean_sample_reviews' collection.")
+    collection.delete_many({})
+    print("Existing data deleted.")
 
 # CSVを読み込む
 df = pd.read_csv("clean_sample.csv")
@@ -15,4 +25,4 @@ df = df.where(pd.notnull(df), None)
 # DataFrameを辞書のリストに変換してMongoDBへ一括挿入
 collection.insert_many(df.to_dict(orient="records"))
 
-print("データをMongoDBにインポートしました。")
+print(f"データをMongoDBにインポートしました。({len(df)}件)")
